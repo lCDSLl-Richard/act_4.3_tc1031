ad79bc6bdd8dcb08e28b66957718d7ad2d6935be
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2022-11-23 11:09:46 -0600

    Activity done

--- exception.h
+++ exception.h
@@ -18,83 +18,98 @@
 #include <cstdio>
 
 /*********** Exception ***********/
-class Exception : public std::exception {
+class Exception : public std::exception
+{
 protected:
 	std::string text;
 
 public:
 	Exception() throw();
 	~Exception() throw();
-	virtual const char* what() const throw();
+	virtual const char *what() const throw();
 };
 
-Exception::Exception() throw ()
-	: text("Basic exception") {
+Exception::Exception() throw()
+		: text("Basic exception")
+{
 }
 
-Exception::~Exception() throw () {}
+Exception::~Exception() throw() {}
 
-const char* Exception::what() const throw() {
+const char *Exception::what() const throw()
+{
 	return text.c_str();
 }
 
 /*********** RangeError ***********/
-class RangeError : public Exception {
+class RangeError : public Exception
+{
 public:
 	RangeError() throw();
 };
 
-RangeError::RangeError() throw () {
-		text = std::string("RangeError");
+RangeError::RangeError() throw()
+{
+	text = std::string("RangeError");
 }
 
 /*********** OutOfMemory ***********/
-class OutOfMemory : public Exception {
+class OutOfMemory : public Exception
+{
 public:
 	OutOfMemory() throw();
 };
 
-OutOfMemory::OutOfMemory() throw () {
-		text = std::string("OutOfMemory");
+OutOfMemory::OutOfMemory() throw()
+{
+	text = std::string("OutOfMemory");
 }
 
 /*********** IndexOutOfBounds ***********/
-class IndexOutOfBounds : public Exception {
+class IndexOutOfBounds : public Exception
+{
 public:
 	IndexOutOfBounds() throw();
 };
 
-IndexOutOfBounds::IndexOutOfBounds() throw () {
+IndexOutOfBounds::IndexOutOfBounds() throw()
+{
 	text = std::string("IndexOutOfBounds");
 }
 
 /*********** NoSuchElement ***********/
-class NoSuchElement : public Exception {
+class NoSuchElement : public Exception
+{
 public:
 	NoSuchElement() throw();
 };
 
-NoSuchElement::NoSuchElement() throw () {
-		text = std::string("NoSuchElement");
+NoSuchElement::NoSuchElement() throw()
+{
+	text = std::string("NoSuchElement");
 }
 
 /*********** IllegalAction ***********/
-class IllegalAction : public Exception {
+class IllegalAction : public Exception
+{
 public:
 	IllegalAction() throw();
 };
 
-IllegalAction::IllegalAction() throw () {
-		text = std::string("IllegalAction");
+IllegalAction::IllegalAction() throw()
+{
+	text = std::string("IllegalAction");
 }
 
 /*********** Overflow ***********/
-class Overflow : public Exception {
+class Overflow : public Exception
+{
 public:
 	Overflow() throw();
 };
 
-Overflow::Overflow() throw () {
-		text = std::string("Overflow");
+Overflow::Overflow() throw()
+{
+	text = std::string("Overflow");
 }
 #endif /* EXCEPTION_H */
